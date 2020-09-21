cd1a1a9873a643670e8ed490ec20b261b3079c01
Sahar Brodbeker <saharonb@gmail.com>
2020-09-21 23:12:03 +0300

    Flag (literally) current user (#3)

--- client/components/Avatar/index.tsx
+++ client/components/Avatar/index.tsx
@@ -1,17 +1,24 @@
 import * as React from 'react';
+import flagIcon from '../../assets/icons/flag.svg';
 import './style.scss';
 
 interface AvatarProps {
     username: string;
     colorIndex: number;
+    showFlag?: boolean;
 }
 
-const Avatar = ({ username, colorIndex }: AvatarProps): React.ReactElement => {
+const Avatar = ({
+    username,
+    colorIndex,
+    showFlag
+}: AvatarProps): React.ReactElement => {
     const initial = username.charAt(0);
 
     return (
         <span className={`avatar flex-center color-${colorIndex}`}>
             { initial }
+            { showFlag && <img src={flagIcon}/> }
         </span>
     );
 };
